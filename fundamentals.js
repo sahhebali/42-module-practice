4ccbac43ba2bf7180eaf702ea256429162fec9b9
sahhebali <saheb.co21@gmail.com>
2021-10-11 09:55:59 +0530

    42-module-pracktice

--- fundamentals.js
+++ fundamentals.js
@@ -1,9 +1,52 @@
 // Task-1 Fundamentals
 
-/* tree type variablse */
+/*1.1 tree type variablse */
 var myName = 'Md Saheb';         /* string type */
 let myAge = 25;                  /* number type:  will be chang*/
 const myBirthOfYear = 1996;      /* const not will be change  */
 const sunWakeUp = ('East', true);  /* boolian type */
 
-/* condition */
+/* 1.2 condition */
+const egg = 100;   /* < lesthan condition */
+const myMony = 50;
+if (egg < myMony) {
+    // console.log('100 takay dile egg kinbo na');
+}
+else {
+    // console.log('50 taka hole kinbo ');
+}
+/* geterthen condition */
+const brotherAge = 25;
+const sisterAge = 21;
+const ageCalculete = brotherAge > sisterAge;
+// console.log(ageCalculete);
+
+/* duble== equal(soman kina ): triple equal=== (check data type) */
+const chiken = 100;
+const beep = 100;
+if (chiken == beep) {
+    console.log('get only chiken , beep')
+}
+else {
+    console.log('get anything')
+}
+
+const appele = 90;
+const orange = "90";
+if (appele === orange) {
+    console.log('md saheb');
+}
+
+// if (new Date().getHours() < 18) {
+//     document.getElementById("demo").innerHTML = "Good day!";
+//   }
+
+// const hour = new Date().getHours();
+// let greeting;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
