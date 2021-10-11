cf50dad0affd5f7fad698cb26b0fc7d262939a81
sahhebali <saheb.co21@gmail.com>
2021-10-11 13:01:04 +0530

    42-module-pracktice

--- fundamentals.js
+++ fundamentals.js
@@ -25,17 +25,110 @@ const ageCalculete = brotherAge > sisterAge;
 const chiken = 100;
 const beep = 100;
 if (chiken == beep) {
-    console.log('get only chiken , beep')
+    // console.log('get only chiken , beep')
 }
 else {
-    console.log('get anything')
+    // console.log('get anything')
 }
 
 const appele = 90;
 const orange = "90";
 if (appele === orange) {
-    console.log('md saheb');
+    // console.log('md saheb');
 }
+
+/* != not equal (dutoi osoman kina ) */
+const iphon = 81000;
+const samsungTab = 81000;
+if (samsungTab != 80000) {
+    // console.log('iphone');
+}
+else {
+    // console.log('samsung tab')
+}
+
+/* <= (bam pase jeta ache seta dan paser theke choto ba soman kina ) */
+const mobilePrice = 15000;
+const iphonePrice = 80000;
+if (mobilePrice <= iphonePrice) {
+    const byingMobile = 'samsung phone kinbo '
+    // console.log(byingMobile)
+}
+else {
+    const byingIphone = 'iphone kinbo';
+    // console.log(byingIphone)
+
+}
+
+
+
+/* >= (bam pase ja ache dan paser theke boro ba soaman kina ) */
+
+const offLineBooks = 100;
+const onlineBooks = 80;
+if (offLineBooks >= 100) {
+    // console.log('offLine books')
+}
+else {
+    // console.log('online books')
+}
+
+/* &&(bam pas ebong dan paser duto sorto puron korte hobe ) */
+
+
+
+const getJob = true;
+const home = false;
+const money = 2500000;
+if (getJob && money >= 300000) {
+    // console.log('tomar chakri & bari thakle tumi allowed  hobe')
+}
+else {
+    // console.log('not allowed')
+}
+
+
+
+/* ||( eitar mane hoche bam ebong dan paser je kono ekta jodi sotto hote hobe) */
+if (getJob || home) {
+    // console.log('omar chakri othoba bari jodi thake tob tumi allowed hobe ')
+}
+else {
+    // console.log('you not allowed')
+}
+
+
+
+
+/* --------------
+1.3 array: declare, length, index, push, pop, indexOf, includes
+ ----------------*/
+/* array declare */
+const mobileNumber = [3010, 8759, 'md saheb', 6456, 'ali', 'mehedi', 2559];
+const includdd = mobileNumber.includes('mehedi')
+const serch = mobileNumber.indexOf('mehedi')
+const last = mobileNumber.lastIndexOf(8759, 3)
+mobileNumber.push(9932);
+mobileNumber.pop()
+const arrayCount = mobileNumber.length;
+const arrayIndex = mobileNumber[3]
+console.log(includdd);
+
+
+
+
+/*-----------
+ 1.4 for loop, while
+ ------------ */
+
+const getPen = 5;
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+
+
+
 
 // if (new Date().getHours() < 18) {
 //     document.getElementById("demo").innerHTML = "Good day!";
