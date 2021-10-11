91a8c3276230cafbedc177acf3413c21fd4816ad
sahhebali <saheb.co21@gmail.com>
2021-10-11 08:50:57 +0530

    42-module-practice

---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+// Task-1 Fundamentals
+
+/* tree type variablse */
+var myName = 'Md Saheb';         /* string type */
+let myAge = 25;                  /* number type:  will be chang*/
+const myBirthOfYear = 1996;      /* const not will be change  */
+const sunWakeUp = ('East', true);  /* boolian type */
+
+/* condition */
